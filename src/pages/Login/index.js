b0a477c7e25d03c808aeea5cd8b222c5fab65e52
Breno Castelo Branco <breno@vizir.com.br>
2019-10-19 17:34:42 -0300

    feat(login-page): add call to api on email submit

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 
+import api from '../../services/api';
+
 export default function Login() {
   const [email, setEmail] = useState('');
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
   }
 
   return (
