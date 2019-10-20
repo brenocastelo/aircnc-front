e8ddd49fdcb3451e1863276c4adea1b1e692c146
Breno Castelo Branco <breno@vizir.com.br>
2019-10-20 15:59:14 -0300

    feat(dashboard-page): get spots from api

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from '../../services/api';
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    async function loadSpots() {
+      const user_id = localStorage.getItem('user');
+      const response = await api.get('/dashboard', {
+        headers: { user_id }
+      });
+
+      setSpots(response.data);
+    }
+
+    loadSpots();
+  }, []);
 
   return (
     <>
@@ -9,7 +24,7 @@ export default function Dashboard() {
         {spots.map(spot => (
           <li key={spot._id}>
             <header />
-            <strong>spot.company</strong>
+            <strong>{spot.company}</strong>
             <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
           </li>
         ))}
